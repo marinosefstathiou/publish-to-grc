14bc73dd03eefc4089c56184c339a4c93eaf9f78
Marios Papaiosif <mp185316@ncr.com>
2023-02-10 10:01:18 +0200

    v0.0.0

--- Program.cs
+++ Program.cs
@@ -34,4 +34,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<DbContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 app.Run();
