2e379e49a4017f068e551ab0f62fca49141683a0
Marios Papaiosif <mp185316@ncr.com>
2023-02-14 10:58:57 +0200

    v7.7.7

---
+++ Migrations/20230214085510_test021420231054.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KubeTestAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class test021420231054 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Temperatures");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Temperatures",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
