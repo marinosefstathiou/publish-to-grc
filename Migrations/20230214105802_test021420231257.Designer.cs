5e23f097bbc709d6c2f7182dc57985591d98668a
Marios Papaiosif <mp185316@ncr.com>
2023-02-14 13:01:33 +0200

    v8.8.8

--- Migrations/20230214105802_test021420231257.Designer.cs
+++ Migrations/20230214105802_test021420231257.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using KubeTestAPI.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace KubeTestAPI.Migrations
 {
     [DbContext(typeof(WeatherDBContext))]
-    partial class WeatherDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230214105802_test021420231257")]
+    partial class test021420231257
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
