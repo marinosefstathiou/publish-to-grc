4b0799cdbc24ad2e5d894c58c14179f0dcabc27f
Marios Papaiosif <mp185316@ncr.com>
2023-02-20 10:46:22 +0200

    sync database

--- Entities/Temperatures.cs
+++ Entities/Temperatures.cs
@@ -7,6 +7,6 @@ namespace KubeTestAPI.Entities
         [Key]
         public string LocationID { get; set; }
         public DateOnly Timestamp { get; set; }
-        public string Country { get; set; }
+        public string Town { get; set; }
     }
 }
---
+++ Migrations/20230220084345_test022020231043.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KubeTestAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class test022020231043 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Town",
+                table: "Temperatures",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Town",
+                table: "Temperatures");
+        }
+    }
+}
